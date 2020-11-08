ae351f154229dadc0f7ac005e831a308c73e8503
Godwithus777 <17801190934@163.com>
2020-11-08 18:18:31 +0800

    7.注册用户

--- foodie-dev-service/src/main/java/com/imooc/service/UserService.java
+++ foodie-dev-service/src/main/java/com/imooc/service/UserService.java
@@ -1,5 +1,8 @@
 package com.imooc.service;
 
+import com.imooc.pojo.Users;
+import com.imooc.pojo.bo.UserBo;
+
 public interface UserService {
 
     /**
@@ -7,6 +10,13 @@ public interface UserService {
      */
 
     public boolean queryUsernameIsExist(String username);
+
+
+    /**
+     * 创建用户
+     */
+
+    public Users createUser(UserBo userBo);
 
 
 }
--- foodie-dev-service/src/main/java/com/imooc/service/impl/UserServiceImpl.java
+++ foodie-dev-service/src/main/java/com/imooc/service/impl/UserServiceImpl.java
@@ -1,19 +1,31 @@
 package com.imooc.service.impl;
 
+import com.imooc.enums.Sex;
 import com.imooc.mapper.UsersMapper;
 import com.imooc.pojo.Users;
+import com.imooc.pojo.bo.UserBo;
 import com.imooc.service.UserService;
+import com.imooc.util.DateUtil;
+import com.imooc.util.MD5Utils;
+import org.n3r.idworker.Sid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import tk.mybatis.mapper.entity.Example;
 
+import java.util.Date;
+
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     private UsersMapper usersMapper;
+
+    @Autowired
+    private Sid sid;
+
+    private static final String USER_FACE = "http://122.152.205.72:88/group1/M00/00/05/CpoxxFw_8_qAIlFXAAAcIhVPdSg994.png";
 
     @Transactional(propagation = Propagation.SUPPORTS)
     @Override
@@ -27,4 +39,40 @@ public class UserServiceImpl implements UserService {
 
         return result == null ? false : true;
     }
+
+    // 创建用户
+    @Transactional(propagation = Propagation.REQUIRED)
+    @Override
+    public Users createUser(UserBo userBo) {
+
+        String userId = sid.nextShort();
+
+        Users user = new Users();
+        user.setId(userId);
+        user.setUsername(userBo.getUsername());
+        try {
+            user.setPassword(MD5Utils.getMD5Str(userBo.getPassword()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // 默认用户昵称同用户名
+        user.setNickname(userBo.getUsername());
+
+        // 默认头像
+        user.setFace(USER_FACE);
+
+        // 默认生日
+        user.setBirthday(DateUtil.stringToDate("1999-01-01"));
+
+        // 默认性别 为保密
+        user.setSex(Sex.secret.type);
+
+        user.setCreatedTime(new Date());
+        user.setUpdatedTime(new Date());
+
+        usersMapper.insert(user);
+
+        return user;
+    }
+
 }
