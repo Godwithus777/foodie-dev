b9d0a1bdf9360f7f96dcdb362d48bf1b1707870d
Godwithus777 <17801190934@163.com>
2020-11-11 11:05:52 +0800

    18.商品推荐获取最新6条商品数据

--- foodie-dev-api/src/test/java/com/test/TransTest.java
+++ foodie-dev-api/src/test/java/com/test/TransTest.java
@@ -1,18 +1,13 @@
 package com.test;
 
 
-import com.imooc.Application;
 import com.imooc.service.StuService;
 import com.imooc.service.TestTransService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 // 事务的测试
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = Application.class)
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(classes = Application.class)
 public class TransTest {
 
     @Autowired
@@ -22,7 +17,7 @@ public class TransTest {
     private TestTransService testTransService;
 
 
-    @Test
+//    @Test
     public void myTest() {
 
         testTransService.testPropagationTrans();
